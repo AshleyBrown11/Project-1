490e439076903f8af03a24f2d3b76f7547a53477
Ashley <ajbrown_88@yahoo.com>
2023-07-28 03:16:53 -0400

    finally got the functions working

--- script.js
+++ script.js
@@ -1,97 +1,101 @@
-// Defining Game Parts
-const Player_1 = "X";
-const Player_2 = "O";
-let currentPlayer = "X";
-
-// Elements
-const cells = document.querySelectorAll(".cell");
-const gameOverText = document.getElementById("game-over-text");
+const cells = Array.from(document.getElementsByClassName("cell"));
 const restartBtn = document.getElementById("restartBtn");
+restartBtn.addEventListener("click", reset);
+let gameOverText = document.getElementById("game-over-text");
+const areas = [null, null, null, null, null, null, null, null, null];
+const o_text = "O";
+const x_text = "X";
+let currentPlayer = o_text;
+let winCellIds = [];
 
-
-// Sounds
-//const clickSound = new Audio("sounds/arcade-click.wav");
-//const youLoseSound = new Audio("sounds/fairytale-game-over.wav");
-
-
-// Initialize Game
-const winCombinations = [
-    // Rows
-    { combo: [0, 1, 2]},
-    { combo: [3, 4, 5]},
-    { combo: [6, 7, 8]},
-    // Columns
-    { combo: [0, 3, 6]},
-    { combo: [1, 4, 7]},
-    { combo: [2, 5, 8]},
-    // Diagonals
-    { combo: [0, 4, 8]},
-    { combo: [2, 4, 6]}
-  ];
-
-  let running = false;
-
-// Game Functions
-startGame();
-function startGame() {
-    cells.forEach(cell => cell.addEventListener("click", cellClick));
-}    
-
-function cellClick(event) {
-    const id = event.target.id;
-    if (!cells[id]) {
-    cells[id] = currentPlayer;
-    event.target.innerText = currentPlayer;
-    }
+function ClickEvent() {
+  cells.forEach((cell) => {
+    cell.addEventListener("click", cellClick);
+  });
 }
 
-checkWinner();
-function checkWinner() {
-    let wonRound = false;
-//   console.log(winCombinations[0].combo);
-    for (let i = 0; i < 8; i++) {
-      const combo = winCombinations[i];
-      const cellA = combo[0];
-      const cellB = combo[1];
-      const cellC = combo[2];
+ClickEvent();
 
-
-      if (cellA == "" || cellB == "" || cellC == "") {
-        continue;
-      }
-      if (cellA == cellB && cellB == cellC) {
-        wonRound = true;
-        break;
-      }
-    }
-
-    if (wonRound) {
-      gameOverText.textContent = `${currentPlayer} wins!`;
-      running = false;
-      
-    } else if (!options.includes("")) {
-      gameOverText.textContent = `Cat Game!`;
-      running = false;
-     
-    } else {
-      changePlayer();
-
-    }
-}
-    function changePlayer() {
-    currentPlayer = (currentPlayer == "X") ? "O" : "X";
-    // console.log(gameOverText)
-    gameOverText.textContent = `${currentPlayer}'s turn`;
+function cellClick(e) {
+  if (winCellIds.length > 0) {
+    return;
   }
+  const id = e.target.id;
+  if (!areas[id]) {
+    areas[id] = currentPlayer;
+    e.target.innerHTML = currentPlayer;
 
-restartBtn.addEventListener("click", resetBoard);
-gameOverText.textContent = `${currentPlayer}'s turn`;
-running = true;
-
-function updateCell(cell,index) {
-    options[index] = currentPlayer;
-    cell.innerText = currentPlayer;
-}
-function resetBoard() {
-    cells.forEach((cell) => (cell.innerText = " "));
+    if (hasPlayerWon(currentPlayer)) {
+      gameOverText.innerText = `${currentPlayer} has won!!`;
+      gameOverText.style.background = "white";
+      changeWinCellsBg();
+      return;
+    }
+    currentPlayer = currentPlayer === o_text ? x_text : o_text;
   }
+}
+
+function hasPlayerWon(cPlayer) {
+  if (areas[0] === cPlayer) {
+    if (areas[1] === cPlayer && areas[2] === cPlayer) {
+      winCellIds = [0, 1, 2];
+      return true;
+    }
+    if (areas[3] === cPlayer && areas[6] === cPlayer) {
+      winCellIds = [0, 3, 6];
+      return true;
+    }
+    if (areas[4] === cPlayer && areas[8] === cPlayer) {
+      winCellIds = [0, 4, 8];
+      return true;
+    }
+  }
+  if (areas[4] === cPlayer) {
+    if (areas[1] === cPlayer && areas[7] === cPlayer) {
+      winCellIds = [4, 1, 7];
+      return true;
+    }
+    if (areas[2] === cPlayer && areas[6] === cPlayer) {
+      winCellIds = [4, 2, 6];
+      return true;
+    }
+    if (areas[3] === cPlayer && areas[5] === cPlayer) {
+      winCellIds = [4, 3, 5];
+      return true;
+    }
+  }
+  if (areas[8] === cPlayer) {
+    if (areas[7] === cPlayer && areas[6] === cPlayer) {
+      winCellIds = [8, 7, 6];
+      return true;
+    }
+    if (areas[5] === cPlayer && areas[2] === cPlayer) {
+      winCellIds = [8, 5, 2];
+      return true;
+    }
+  }
+}
+
+function changeWinCellsBg() {
+  winCellIds.forEach((id) => {
+    cells[id].style.background = "white";
+  });
+  cells.forEach((cell) => {
+    cell.style.cursor = "not-allowed";
+  });
+}
+
+function reset() {
+  winCellIds = [];
+  areas.forEach((val, index) => {
+    areas[index] = null;
+  });
+  cells.forEach((cell) => {
+    cell.innerHTML = "";
+    cell.style.background = "";
+    cell.style.cursor = "pointer";
+  });
+  gameOverText.innerHTML = "Let's play..";
+  gameOverText.style.background = "";
+  currentPlayer = o_text;
+}
